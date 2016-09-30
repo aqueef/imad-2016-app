eb698c5f59ab1e59a4657947d90b9d2514dcfd5f
aqueef <aqueef.danyal89@gmail.com>
2016-09-30 17:25:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 // counter code
 var button=document.getElementById('counter');
 var counter=0;
-button.oneclick= function() {
+button.onclick= function() {
   var request= new XMLHttpRequest();
   request.onreadystatechange = function() {
       if (request.readystate === XMLHttpRequest.Done) {
