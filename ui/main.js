6b38a9e513c43f6a3b9b699e2f8070bd7778a4e7
aqueef <aqueef.danyal89@gmail.com>
2016-09-30 14:34:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,19 @@
 var button=document.getElementById('counter');
 var counter=0;
 button.oneclick= function() {
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML = counter.toString();
-    
+  var request= new XMLHttpRequest();
+  request.onreadystatechange = function() {
+      if (request.readystate === XMLHttpRequest.Done) {
+          if(request.status===200) {
+              var counter= request.responseText;
+              var span=document.getElementById('count');
+              span.innerHTML =counter.toString();
+          }
+      
+  }
+  
+};
+
+request.open('GET', 'http://aqueef.imad.hasura-app.io/counter', true);
+request.send(null);
 };
