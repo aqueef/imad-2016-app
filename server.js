8c6b7b26cd0730051af62698138e386b142423d0
aqueef <aqueef.danyal89@gmail.com>
2016-10-01 14:45:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,6 +84,13 @@ app.get('/counter', function(req,res) {
     counter =counter+1;
     res.send(counter.toString());
 }); 
+var names=[];
+app.get('/submit-name' , function(req,res) {
+    var name=req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
 
 
 app.get('/:articlename', function (req, res) {
@@ -103,12 +110,6 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-var names=[];
-app.get('/submit-name' , function(req,res) {
-    var name=req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
 });
 
 
