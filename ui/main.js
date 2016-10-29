f7c887e38f5869c55de37ff246b4f2316217227c
aqueef <aqueef.danyal89@gmail.com>
2016-10-29 19:12:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,46 +1,11 @@
 // counter code
-var button=document.getElementById('counter');
-var counter=0;
-button.onclick= function() {
-  var request= new XMLHttpRequest();
-  request.onreadystatechange = function() {
-      if (request.readyState === XMLHttpRequest.DONE) {
-          if(request.status===200) {
-              var counter= request.responseText;
-              var span=document.getElementById('count');
-              span.innerHTML =counter.toString();
-          }
-      
-  }
-  
-};
 
-request.open('GET', 'http://aqueef.imad.hasura-app.io/counter', true);
-request.send(null);
-};
-// Submit name
 
-var submit=document.getElementById('submit_btn');
-submit.onclick=function () {
-     var request= new XMLHttpRequest();
-  request.onreadystatechange = function() {
-      if (request.readyState === XMLHttpRequest.DONE) {
-          if(request.status===200) {
-              
-        var names=request.responseText;
-        names=JSON.parse(names);
-    var list='';
-    for(var i=0;i<names.length;i++){
-    list += '<li>'+names[i]+'</li>';
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
-var ul=document.getElementById('namelist');
-ul.innerHTML=list;
-
-}
-}
-};
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
-request.open('GET', 'http://aqueef.imad.hasura-app.io/submit-name?name='+ name, true);
-request.send(null);
-};
